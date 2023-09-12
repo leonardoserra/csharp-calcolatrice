655414757536d781d0f6b84af86e927c17c9c424
leonardoserra <serra.leonardo92@gmail.com>
2023-09-12 14:44:53 +0200

    creata sottrazione e moltiplicazione interi e double

--- csharp-calcolatrice/Calculator.cs
+++ csharp-calcolatrice/Calculator.cs
@@ -10,14 +10,32 @@ namespace csharp_calcolatrice
     {
         public static int IntegerSum(int firstAddend, int secondAddend)
         {
-                    return firstAddend + secondAddend;
-           
+            return firstAddend + secondAddend;
         }
 
         public static double DoubleSum(double firstAddend, double secondAddend)
         {
             return firstAddend + secondAddend;
+        }
 
+        public static int IntegerSubtractor(int firstSubtracting, int secondSubtracting)
+        {
+            return firstSubtracting - secondSubtracting;
+        }
+
+        public static double DoubleSubtractor(double firstSubtracting, double secondSubtracting)
+        {
+            return firstSubtracting - secondSubtracting;
+        }
+
+        public static int IntegerMultiplier(int firstMultiply, int secondMultiply)
+        {
+            return firstMultiply * secondMultiply;
+        }
+
+        public static double DoubleMultiplier(double firstMultiply, double secondMultiply)
+        {
+            return firstMultiply * secondMultiply;
         }
     }
 }
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -4,8 +4,14 @@
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine(Calculator.IntegerSum(2, 8));
             Console.WriteLine(Calculator.DoubleSum(2.3, 8));
+            Console.WriteLine(Calculator.IntegerSubtractor(2, 8));
+            Console.WriteLine(Calculator.DoubleSubtractor(2.3, 8));
+            Console.WriteLine(Calculator.IntegerMultiplier(2, 8));
+            Console.WriteLine(Calculator.DoubleMultiplier(2.3, 8));
+
 
         }
     }
