b9bba3ce3d61b37a6689c972132e7a765a6bcbf1
leonardoserra <serra.leonardo92@gmail.com>
2023-09-12 14:26:51 +0200

    creata IntegerSum()

---
+++ csharp-calcolatrice/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal static class Calculator
+    {
+        public static int IntegerSum(int firstAddend, int secondAddend)
+        {
+           
+                if(firstAddend is Int32 && secondAddend is Int32) 
+                    return firstAddend + secondAddend;
+                else
+                    throw new Exception("Use only Integer");
+            
+        }
+    }
+}
